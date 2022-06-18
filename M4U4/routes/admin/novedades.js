231a048a37703412bda2849604c53dd7d26d1764
veronica silva <veronica_silva77@outlook.com>
2022-06-18 13:44:19 -0300

    tarea M4U4

---
+++ M4U4/routes/admin/novedades.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('admin/novedades', {
+      layout:'admin/layout',
+      usuario: req.session.nombre, //veronica
+   });
+});
+
+module.exports = router;
